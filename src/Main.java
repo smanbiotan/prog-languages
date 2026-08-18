403db60b58fbcf09111bfe1667c1dca67868761b
sher <manbiotansherwinalvin@gmail.com>
2026-08-18 16:00:54 +0800

    Add java activity

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee employee1 = new Employee("Riel AI", "EMP-001", "Software Engineer", 95000);
+        Employee employee2 = new Employee("Moca AI", "EMP-002", "Ai Automation Specialist", 85000);
+
+        employee1.displayInfo();
+        employee2.displayInfo();
+    }
+}
