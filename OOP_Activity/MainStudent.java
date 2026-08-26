33153f3a29493c349bf8820fe80539ba2114dc86
sher <manbiotansherwinalvin@gmail.com>
2026-08-26 22:16:14 +0800

    Add OOP Activity

---
+++ OOP_Activity/MainStudent.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter student name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter Prelim Grade: ");
+        float prelim = input.nextFloat();
+
+        System.out.print("Enter Midterm Grade: ");
+        float midterm = input.nextFloat();
+
+        System.out.print("Enter Final Grade: ");
+        float finals = input.nextFloat();
+
+        Student studKirk = new Student(name, prelim, midterm, finals);
+
+        studKirk.displayStudentInfo();
+        studKirk.getStatus();
+        studKirk.getLetterGrade();
+
+    }
+
+
+}
