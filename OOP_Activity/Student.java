2b89a82bbcb9f94306ab910cba8114d3c47c2366
sher <manbiotansherwinalvin@gmail.com>
2026-08-31 12:07:07 +0800

    Add OOP Activity

--- OOP_Activity/Student.java
+++ OOP_Activity/Student.java
@@ -12,31 +12,31 @@ public class Student {
         this.finals = finals;
     }
 
-    public float calculateAverage() {
+    public double calculateAverage() {
 
-        return (float) (prelim + midterm + finals) / 3;
+        return (prelim + midterm + finals) / 3;
     }
 
-    public void getStatus() {
+    public String getStatus() {
 
         if(calculateAverage() >= 75) {
-            System.out.println("Status: PASSED");
+            return "Status: PASSED";
         } else {
-            System.out.println("Status: FAILED");
+            return "Status: FAILED";
         }
 
     }
 
-    public void getLetterGrade() {
+    public String getLetterGrade() {
 
         if(calculateAverage() >= 95) {
-            System.out.println("Letter Grade: 'A'" );
+            return "A+";
         } else if(calculateAverage() >= 85) {
-            System.out.println("Letter Grade: 'B'" );
+            return "A- ";
         } else if(calculateAverage() >= 75) {
-            System.out.println("Letter Grade: 'C' " );
+            return "B ";
         } else {
-            System.out.println("Letter Grade: 'F'" );
+            return "F ";
         }
     }
 
@@ -44,10 +44,9 @@ public class Student {
 
         System.out.println("\n---STUDENT FINAL GRADE---");
         System.out.println("Name: " + name);
-        System.out.println("Prelim: " + prelim);
-        System.out.println("Midterm: " + midterm);
-        System.out.println("Finals: " + finals);
-        System.out.println("Average: " + calculateAverage());
+        System.out.printf("Average: %.2f%n", calculateAverage());
+        System.out.println("Status: " + getStatus());
+        System.out.println("Letter Grade: " + getLetterGrade());
     }
 
 }
